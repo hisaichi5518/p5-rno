26d170c13ed4496b055d1cf774d115bc08d5d3ed
hisaichi5518 <hisaichi5518@gmail.com>
2014-06-21 19:03:23 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Rno - It's new $module
+
+# SYNOPSIS
+
+    use Rno;
+
+# DESCRIPTION
+
+Rno is ...
+
+# LICENSE
+
+Copyright (C) hisaichi5518.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+hisaichi5518 <hisaichi5518@gmail.com>
---
+++ lib/Rno.pm
@@ -0,0 +1,39 @@
+package Rno;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Rno - It's new $module
+
+=head1 SYNOPSIS
+
+    use Rno;
+
+=head1 DESCRIPTION
+
+Rno is ...
+
+=head1 LICENSE
+
+Copyright (C) hisaichi5518.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+hisaichi5518 E<lt>hisaichi5518@gmail.comE<gt>
+
+=cut
+
