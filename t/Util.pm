a38eb319cee935fd6e76b1bafb9ed362157c135b
hisaichi5518 <hisaichi5518@gmail.com>
2014-06-22 11:46:37 +0900

    Added setup_database.

--- t/Util.pm
+++ t/Util.pm
@@ -2,6 +2,12 @@ package t::Util;
 use strict;
 use warnings;
 use utf8;
+use parent "Exporter";
+use DBIx::Sunny;
+
+our @EXPORT = qw(
+    setup_database
+);
 
 sub import {
     my ($class, @args) = @_;
@@ -17,6 +23,28 @@ sub import {
 
     require Test::Deep::Matcher;
     Test::Deep::Matcher->export_to_level(1);
+
+    $class->export_to_level(1, $class, @args);
 }
 
+my $mysqld;
+sub setup_database {
+    $mysqld = Test::mysqld->new(
+        my_cnf => {
+            'skip-networking' => '',
+        },
+    ) or return;
+
+    my $dbh = DBIx::Sunny->connect($mysqld->dsn(dbname => "test"));
+
+    $dbh->do(q{
+        CREATE TABLE foo (
+            id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+            name VARCHAR(10)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+    });
+    $dbh->query(q{INSERT INTO foo (name) VALUES(?)}, "is name");
+
+    $mysqld;
+}
 1;
